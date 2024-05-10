018d90dbc4e7f459a1bc76d8109380040c43d379
tuiooo8 <tanktravispun0suy@hotmail.com>
2024-05-10 13:37:45 +0700

    test

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-function fibonacci(n) {
-  if (n <= 1) return n;
-  return fibonacci(n - 1) + fibonacci(n - 2);
+function pathSum(root, sum) {
+  if (!root) return false;
+  if (!root.left && !root.right && root.val === sum) return true;
+  return (
+    pathSum(root.left, sum - root.val) || pathSum(root.right, sum - root.val)
+  );
 }
