48e98afdf5c2bfd2d7c7335ba3f85734be68237c
tuiooo8 <tanktravispun0suy@hotmail.com>
2024-05-10 13:43:52 +0700

    hello world commit

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-function pathSum(root, sum) {
-  if (!root) return false;
-  if (!root.left && !root.right && root.val === sum) return true;
-  return (
-    pathSum(root.left, sum - root.val) || pathSum(root.right, sum - root.val)
-  );
-}
+const pullAtValue = (arr, pullArr) => {
+  let removed = [],
+    pushToRemove = arr.forEach((v, i) =>
+      pullArr.includes(v) ? removed.push(v) : v,
+    ),
+    mutateTo = arr.filter((v, i) => !pullArr.includes(v));
+  arr.length = 0;
+  mutateTo.forEach((v) => arr.push(v));
+  return removed;
+};
